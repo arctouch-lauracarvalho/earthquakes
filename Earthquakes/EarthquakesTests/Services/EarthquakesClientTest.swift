a101e913505647c5ad96e40b45553c8a3fa1804e
Laura Carvalho <laura.carvalho@arctouch.com>
2024-06-04 12:10:35 -0300

    Extract constant value to a variable

--- Earthquakes/EarthquakesTests/Services/EarthquakesClientTest.swift
+++ Earthquakes/EarthquakesTests/Services/EarthquakesClientTest.swift
@@ -11,6 +11,8 @@ import XCTest
 final class EarthquakesClientTests: XCTestCase {
     private var sut: EarthquakesClient!
     
+    private let mockURL = "https://example.com/v1"
+    
     override func tearDown() {
         sut = nil
         super.tearDown()
@@ -18,10 +20,10 @@ final class EarthquakesClientTests: XCTestCase {
     
     // MARK: - Instantiation Tests
     func testGivenClientCreated_ThenHostAndNetworkSessionIsSet() {
-        let host = URL(string: "https://example.com/v1")!
+        let host = URL(string: mockURL)!
         let networkSession: NetworkSession = URLSession.shared
         
-        sut = setupClient(hostValue: "https://example.com/v1", networkSession: networkSession)
+        sut = setupClient(hostValue: mockURL, networkSession: networkSession)
         XCTAssertEqual(sut.host, host)
         XCTAssertTrue(sut.networkSession === networkSession)
     }
@@ -33,7 +35,7 @@ final class EarthquakesClientTests: XCTestCase {
     
     // MARK: - Success Tests
     func testGivenEarthquakesClient_WhenFetchingEarthquakesDataAndItSucceeds_ThenEarthquakeListHas2EarthquakeData() async {
-        let mockResponse = responseFor(url: "https://example.com/v1")
+        let mockResponse = responseFor(url: mockURL)
         let mockData = try! Data.fromJSON(fileName: "EarthquakeListData")
         let networkSession = setupNetworkSession(using: mockData, response: mockResponse)
         
@@ -83,7 +85,7 @@ final class EarthquakesClientTests: XCTestCase {
     
     func testGivenEarthquakesClient_WhenFetchingEarthquakesDataAndResponseCodeIsOtherThan200_ThenThrowServerError() async {
         var expectedError: SeismicAPIClientError = .invalidJSON
-        let mockResponse = responseFor(url: "https://example.com/v1", statusCode: 500)
+        let mockResponse = responseFor(url: mockURL, statusCode: 500)
         let networkSession = setupNetworkSession(response: mockResponse)
         
         sut = setupClient(networkSession: networkSession)
@@ -100,7 +102,7 @@ final class EarthquakesClientTests: XCTestCase {
     func testGivenEarthquakesClient_WhenFetchingEarthquakesDataAndJSONIsInvalid_ThenThrowInvalidJSONError() async {
         var expectedError: SeismicAPIClientError = .noInternet
         let mockData = try! Data.fromJSON(fileName: "InvalidEarthquakeData")
-        let mockResponse = responseFor(url: "https://example.com/v1", statusCode: 200)
+        let mockResponse = responseFor(url: mockURL, statusCode: 200)
         let networkSession = setupNetworkSession(using: mockData, response: mockResponse)
         
         sut = setupClient(networkSession: networkSession)
