2b59f278322b2b7d90ab848e1ec7e7187cab59a7
Laura Carvalho <laura.carvalho@arctouch.com>
2024-06-04 18:51:12 -0300

    Create EarthquakeListViewModel
    And move EarthquakeTests to a Model folder for code organization

---
+++ Earthquakes/Earthquakes/ViewModel/EarthquakeListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  EarthquakeListViewModel.swift
+//  Earthquakes
+//
+//  Created by Laura Carvalho on 04/06/24.
+//
+
+import Foundation
+
+final class EarthquakeListViewModel {}
---
+++ Earthquakes/EarthquakesTests/ViewModel/EarthquakeListViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  EarthquakeListViewModelTests.swift
+//  EarthquakesTests
+//
+//  Created by Laura Carvalho on 04/06/24.
+//
+
+import XCTest
+@testable import Earthquakes
+
+final class EarthquakeListViewModelTests: XCTestCase {
+    private var sut: EarthquakeListViewModel!
+}
