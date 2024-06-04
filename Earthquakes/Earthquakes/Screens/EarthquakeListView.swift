46d8d71a519a8c6c6781465d3905d9109952de39
Laura Carvalho <laura.carvalho@arctouch.com>
2024-06-04 18:53:18 -0300

    Replace ContentView by EarthquakeListView

---
+++ Earthquakes/Earthquakes/Screens/EarthquakeListView.swift
@@ -0,0 +1,18 @@
+//
+//  EarthquakeListView.swift
+//  Earthquakes
+//
+//  Created by Laura Carvalho on 04/06/24.
+//
+
+import SwiftUI
+
+struct EarthquakeListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    EarthquakeListView()
+}
