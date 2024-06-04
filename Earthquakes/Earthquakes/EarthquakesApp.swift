9a14643297b776f04dca7d5e6e5d33c5b39dbb8b
Laura Carvalho <laura.carvalho@arctouch.com>
2024-06-03 21:43:02 -0300

    Create new project

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Laura Carvalho on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Earthquakes/EarthquakesTests/EarthquakesTests.swift
@@ -0,0 +1,16 @@
+//
+//  EarthquakesTests.swift
+//  EarthquakesTests
+//
+//  Created by Laura Carvalho on 03/06/24.
+//
+
+import XCTest
+@testable import Earthquakes
+
+final class EarthquakesTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+}
